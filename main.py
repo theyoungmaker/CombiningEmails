0d953f94d20359c3adea902ae52171f390e868c6
Jack Chen <jackchenyujie.96@gmail.com>
2023-07-14 22:34:35 +0800

    answer

--- main.py
+++ main.py
@@ -9,3 +9,17 @@
 # Hint 1 (Reading lines in a file): https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint 2 (Replace a string with another string): https://www.w3schools.com/python/ref_string_replace.asp
 # Hint 3 (remove whitespaces): https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER = "[name]"
+
+with open("./Contacts/names.txt", "r") as file:
+    names = file.readlines()
+
+
+with open("./Email/draft.txt", "r") as draft:
+    draft_letter = draft.read()
+    for name in names:
+        name = name.strip()
+        letter = draft_letter.replace(PLACEHOLDER, name)
+        with open(f"./Send/Letter_for_{name}.txt", "w") as new_file:
+            new_file.write(letter)
