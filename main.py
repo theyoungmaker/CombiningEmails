4e84c66ca24785210230369d5b0952c8ed5421cc
Jack Chen <jackchenyujie.96@gmail.com>
2023-07-14 22:20:16 +0800

    Add starting skeleton

---
+++ main.py
@@ -0,0 +1,11 @@
+# Create a letter using the draft.txt in the Email folder/directory
+# For each name in names.txt in Contacts
+# Replace the [name] placehoder in the draft.txt with the real name
+# Save the finished letters for each name in Send folder
+# You can name the file Letter_for_name.txt where name is the name 
+# the person
+
+
+# Hint 1 (Reading lines in a file): https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint 2 (Replace a string with another string): https://www.w3schools.com/python/ref_string_replace.asp
+# Hint 3 (remove whitespaces): https://www.w3schools.com/python/ref_string_strip.asp
